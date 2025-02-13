411d39cf364c73414eb409c375cd0f78eae2e3a3
krishna yadav <krishnaydv2004@gmail.com>
2025-02-13 19:11:50 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountid = 14456
+let accountemail ="krishna@gamil.com"
+var accountpassword ="12563" 
+accountcity ="gaziabadi" 
+let accountstate;
+
+// accountid =2556 - not allowed
+accountemail ="skdn@gamil.com"
+accountpassword ="54515"
+accountcity ="uttar pradesh"
+
+console.log(accountid);
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountid, accountemail, accountpassword, accountcity, accountstate]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use srict"; // treat all js code as newer version
+
+// alert(3+3) //we are using node.js not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("krishna");
+
+let name = "krishna";
+let age = 18;
+let isloggedin = false;
+let state;
+
+//DATA TYPES:
+
+// number => 2 to the power 53
+// bigint => for larger no. company uses like MAANG
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
