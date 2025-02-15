3259698abec94060ea4d876ea217c711f468c9fa
krishna yadav <krishnaydv2004@gmail.com>
2025-02-15 19:14:07 +0000

    conversion of datadtypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,21 @@
+let score = false
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+//"33" => 33
+//"33abc" =>NaN
+//true =>1; or vice-versa
+
+let isloggedin = "krishna"
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+//1 => true; 0 => false
+// "" => false
+// "krishna" => false
