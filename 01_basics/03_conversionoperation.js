97900a7a3db71d2a4bc913a8ff2e70e9f6c07aaf
krishna yadav <krishnaydv2004@gmail.com>
2025-02-18 19:07:06 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,11 @@
 let score = false
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueinnumber = Number(score)
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 
 //"33" => 33
 //"33abc" =>NaN
@@ -14,8 +14,55 @@ console.log(valueinnumber);
 let isloggedin = "krishna"
 
 let booleanisloggedin = Boolean(isloggedin)
-console.log(booleanisloggedin)
+// console.log(booleanisloggedin)
 
 //1 => true; 0 => false
 // "" => false
 // "krishna" => false
+
+let somenumber = 33
+
+let Stringnumber = String(somenumber)
+// console.log(Stringnumber);
+// console.log(typeof stringnumber);
+
+
+//*******************Operation************
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " krishna"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter);
+
+// link to study
+// https://tc39.es/ecma262/2023/multipage/abstract-operations.html#sec-abstract-operations
+
+
